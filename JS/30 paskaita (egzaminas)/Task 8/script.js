1937cab7bf6744b0d1699546edb41b7baf2e2f87
Mantvydas <mantvydas.dirgincius@icloud.comm>
2022-04-04 08:35:03 +0300

    task 8 done

--- JS/30 paskaita (egzaminas)/Task 8/script.js
+++ JS/30 paskaita (egzaminas)/Task 8/script.js
@@ -5,3 +5,28 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator() {
+  this.sum = function (num1, num2) {
+    return num1 + num2;
+  };
+  this.subtraction = function (num1, num2) {
+    return num1 - num2;
+  };
+  this.multiplication = function (num1, num2) {
+    return num1 * num2;
+  };
+  this.division = function (num1, num2) {
+    return num1 / num2;
+  };
+}
+
+// const calc = new Calculator();
+// const sum = calc.sum(10, 20);
+// console.log("sum ===", sum);
+// const atimtis = calc.subtraction(10, 20);
+// console.log("atimtis ===", atimtis);
+// const daugyba = calc.multiplication(10, 20);
+// console.log("daugyba ===", daugyba);
+// const dalyba = calc.division(10, 20);
+// console.log("dalyba ===", dalyba);
